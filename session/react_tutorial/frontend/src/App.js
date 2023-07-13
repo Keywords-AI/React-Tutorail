782603a17f6611d9693a421c62516d36dc2f7100
Raymond1415926 <1415176717@qq.com>
2023-07-13 17:46:41 -0400

    update readme convensions

---
+++ session/react_tutorial/frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{
+        color: 'white',
+    }}>App</div>
+  )
+}
---
+++ session/react_tutorial/frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve( __dirname, 'static/frontend/js' ),
+        filename: 'index.js',
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true
+     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_module/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css?$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|j?g|svg|gif)?$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve( __dirname, 'public/index.html' ),
+            filename: 'index.html'
+        })
+    ],
+    resolve: {
+        extensions: [ '*', '.js', '.jsx' ],
+        alias: {
+            src: path.resolve( __dirname, 'src' ),
+            root: __dirname,
+        }
+    }
+};
